dd420932ff6ba4863e978303ef15fbc9ec9a72b3
zplato <zach.plato@gmail.com>
2025-03-09 17:15:17 -0400

    init repo

---
+++ TicTacToe.cpp
@@ -0,0 +1,19 @@
+
+// Standard Libraries
+#include <iostream>
+#include <ctime> 
+
+
+
+// Function Declarations
+void drawBoard(char* spaces);
+void playerMove(char* spaces, char player); 
+void AIMove(char* spaces, char player);
+bool checkWinner(char *spaces, char player, char computer); 
+bool checkTie(char *spaces); 
+
+
+int main()
+{
+	return 0; 
+}
